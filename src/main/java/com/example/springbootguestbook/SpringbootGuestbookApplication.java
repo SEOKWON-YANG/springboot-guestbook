d70688f7c8e2660cc143cdf4f53924f590816106
SEOKWON-YANG <namugocps@gmail.com>
2022-01-17 23:48:56 +0900

    스프링 guestbook #6
    스프링 guestbook #5
    
    - 엔티티 클래스와 Querydsl 설정

--- src/main/java/com/example/springbootguestbook/SpringbootGuestbookApplication.java
+++ src/main/java/com/example/springbootguestbook/SpringbootGuestbookApplication.java
@@ -2,8 +2,10 @@ package com.example.springbootguestbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringbootGuestbookApplication {
 
     public static void main(String[] args) {
