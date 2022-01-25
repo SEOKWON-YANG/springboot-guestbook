f830d798d1e70592506ce144de06c9a558dbf738
SEOKWON-YANG <namugocps@gmail.com>
2022-01-25 23:52:25 +0900

    스프링 guestbook #14
    스프링 guestbook #14
    
    - Service 인터페이스 default

--- src/main/java/com/example/springbootguestbook/service/GuestbookService.java
+++ src/main/java/com/example/springbootguestbook/service/GuestbookService.java
@@ -1,7 +1,18 @@
 package com.example.springbootguestbook.service;
 
 import com.example.springbootguestbook.dto.GuestbookDTO;
+import com.example.springbootguestbook.entity.Guestbook;
 
 public interface GuestbookService {
     Long register(GuestbookDTO dto);
+
+    default Guestbook dtoToEntity(GuestbookDTO dto){
+        Guestbook entity = Guestbook.builder()
+                .gno(dto.getGno())
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .writer(dto.getWriter())
+                .build();
+        return entity;
+    }
 }
