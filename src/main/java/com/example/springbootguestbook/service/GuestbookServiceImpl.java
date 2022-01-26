ae3d3d286ab7bd1c679588a25a7fc0fd107b3f21
SEOKWON-YANG <namugocps@gmail.com>
2022-01-26 23:24:19 +0900

    스프링 guestbook #15
    스프링 guestbook #15
    
    - GuestbookDTO 변환

--- src/main/java/com/example/springbootguestbook/service/GuestbookServiceImpl.java
+++ src/main/java/com/example/springbootguestbook/service/GuestbookServiceImpl.java
@@ -1,11 +1,24 @@
 package com.example.springbootguestbook.service;
 
 import com.example.springbootguestbook.dto.GuestbookDTO;
+import com.example.springbootguestbook.entity.Guestbook;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
 
+@Service
+@Log4j2
 public class GuestbookServiceImpl implements GuestbookService{
 
     @Override
     public Long register(GuestbookDTO dto){
+
+        log.info("DTO-------");
+        log.info(dto);
+
+        Guestbook entity = dtoToEntity(dto);
+
+        log.info(entity);
+        
         return null;
     }
 }
