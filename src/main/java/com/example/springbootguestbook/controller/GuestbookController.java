1c181416c247c009dddcefef1b525bd02232a738
SEOKWON-YANG <namugocps@gmail.com>
2022-02-10 23:01:17 +0900

    스프링 guestbook #23
    스프링 guestbook #23
    
    - GuestbookController 클래스 기능 추가

--- src/main/java/com/example/springbootguestbook/controller/GuestbookController.java
+++ src/main/java/com/example/springbootguestbook/controller/GuestbookController.java
@@ -1,20 +1,34 @@
 package com.example.springbootguestbook;
 
+import com.example.springbootguestbook.dto.PageRequestDTO;
+import com.example.springbootguestbook.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor
 public class GuestbookController {
 
-    @GetMapping({"/","/list"})
-    public String list(){
+    private final GuestbookService service; //final로 선언
+
+    @GetMapping({"/"})
+    public String index(){
 
         log.info("list...");
 
-        return "/guestbook/list";
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+        log.info("list...." + pageRequestDTO);
+
+        model.addAttribute("result", service.getList(pageRequestDTO));
     }
 }
